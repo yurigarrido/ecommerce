72ca8ea842786cda7110305855117e495ac26b91
yurigarrido <y.garrido.yuri@gmail.com>
2021-12-27 15:45:06 -0300

    add eslint and prettier

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test(' ', () => {
   render(<App />);
   const linkElement = screen.getByText(/hello world/i);
   expect(linkElement).toBeInTheDocument();
