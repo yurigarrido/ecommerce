1960386daf91055d9818c9d0ab34f3b2ee3ca581
yurigarrido <y.garrido.yuri@gmail.com>
2022-01-09 23:12:52 -0300

    -nabrancherrada!! aprendendo testes com context-api

--- app/src/componets/Input/Input.test.js
+++ app/src/componets/Input/Input.test.js
@@ -1,12 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import renderWithRouter from '../../helper/renderWithRouter';
+import { GlobalStorage } from '../../context/GlobalContext';
 
 import Input from './Input';
 
 describe('<Input />', () => {
   beforeEach(() => {
     renderWithRouter(
-      <Input placeholder="Digite algum termo de pesquisa ou escolha uma categoria" />
+      <GlobalStorage>
+        <Input placeholder="Digite algum termo de pesquisa ou escolha uma categoria" />
+      </GlobalStorage>
     );
   });
 
